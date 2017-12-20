cd9ab8d2425d393a79ba8da69d084f23af8bc308
Murad Rahimov <rahimov.mu@gmail.com>
2017-12-19 19:25:54 -0500

    complete part01

--- app/src/main/java/nyc/c4q/unit04mid_unitassessment/MainActivity.java
+++ app/src/main/java/nyc/c4q/unit04mid_unitassessment/MainActivity.java
@@ -1,5 +1,7 @@
 package nyc.c4q.unit04mid_unitassessment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        MainFragment fragment = new MainFragment();
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/nyc/c4q/unit04mid_unitassessment/MainFragment.java
@@ -0,0 +1,50 @@
+package nyc.c4q.unit04mid_unitassessment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    View rootView;
+    Button buttonMainFrag;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        buttonMainFrag = rootView.findViewById(R.id.button_mainfragment);
+
+        buttonMainFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DisplayFragment displayFragment = new DisplayFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+
+
+                fragmentTransaction.replace(R.id.fragment_container, displayFragment).addToBackStack("Murad").commit();
+            }
+        });
+
+        return rootView;
+    }
+
+}
